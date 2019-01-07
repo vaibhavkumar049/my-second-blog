b7288c671b966526841ceb619297ddbbcf486ef3
Vaibhav Kumar chaudhary <vaibhavkumar049@gmai.com>
2019-01-07 12:17:24 +0530

    added template

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.conf import settings
 
 class Post(models.Model):
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title= models.CharField(max_length=200)
     text=models.TextField()
     created_date=models.DateTimeField(default=timezone.now)
